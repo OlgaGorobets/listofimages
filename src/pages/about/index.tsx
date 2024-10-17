0e83cca6787b780303419dd2e003562d3dc30eac
olga <olga.gorobec@cosysoft.ru>
2024-10-17 11:27:48 +0300

    supabase_addition

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const About = () => {
+  return <Heading>page about</Heading>;
+};
+
+export default About;
+
+const Heading = styled.h1`
+  font-size: 2rem;
+  font-weight: 800;
+  text-decoration: underline;
+`;
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Home = () => {
+  return <Heading>main page</Heading>;
+};
+
+export default Home;
+
+const Heading = styled.h1`
+  font-size: 2rem;
+  font-weight: 800;
+  text-decoration: underline;
+`;
